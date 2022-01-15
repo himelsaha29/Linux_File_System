954d54609ac0506be16ba450358a53f6af3a11c5
himelsaha29 <himelsaha29@live.com>
2022-01-14 22:17:08 -0500

    Indentation fixed

--- sfs_api.c
+++ sfs_api.c
@@ -62,55 +62,55 @@ typedef struct {
 
 void mksfs(int) {
 	
-if(fresh == 1)  { // create file system
+	if(fresh == 1)  { // create file system
 
 
-  	for (int i = 0; i < 500; i++) { // populate FDTable
-  		fdtable[i].inodeNumber = -1;
-  		fdtable[i].rwpointer = -1;
- 	}
+  		for (int i = 0; i < 500; i++) { // populate FDTable
+  			fdtable[i].inodeNumber = -1;
+  			fdtable[i].rwpointer = -1;
+ 		}
 
 
-  	for (int i = 0; i < 500; i++) {
-  		iNodeTable[i].mode = -1;
-  		iNodeTable[i].linkCount = -1;
-  		iNodeTable[i].uid = -1;
-  		iNodeTable[i].gid = -1;
-  		iNodeTable[i].size = -1;
-        	iNodeTable[i].pointer1 = -1;
-        	iNodeTable[i].pointer2 = -1;
-        	iNodeTable[i].pointer3 = -1;
-        	iNodeTable[i].pointer4 = -1;
-        	iNodeTable[i].pointer5 = -1;
-        	iNodeTable[i].pointer6 = -1;
-        	iNodeTable[i].pointer7 = -1;
-        	iNodeTable[i].pointer8 = -1;
-        	iNodeTable[i].pointer9 = -1;
-        	iNodeTable[i].pointer10 = -1;
-        	iNodeTable[i].pointer11 = -1;
-        	iNodeTable[i].pointer12 = -1;
-  		iNodeTable[i].indPointer = -1;
+  		for (int i = 0; i < 500; i++) {
+  			iNodeTable[i].mode = -1;
+  			iNodeTable[i].linkCount = -1;
+  			iNodeTable[i].uid = -1;
+  			iNodeTable[i].gid = -1;
+  			iNodeTable[i].size = -1;
+        		iNodeTable[i].pointer1 = -1;
+        		iNodeTable[i].pointer2 = -1;
+        		iNodeTable[i].pointer3 = -1;
+        		iNodeTable[i].pointer4 = -1;
+        		iNodeTable[i].pointer5 = -1;
+        		iNodeTable[i].pointer6 = -1;
+        		iNodeTable[i].pointer7 = -1;
+        		iNodeTable[i].pointer8 = -1;
+        		iNodeTable[i].pointer9 = -1;
+        		iNodeTable[i].pointer10 = -1;
+        		iNodeTable[i].pointer11 = -1;
+        		iNodeTable[i].pointer12 = -1;
+  			iNodeTable[i].indPointer = -1;
+
+  		}
+
+      		for (int i = 0; i < 500; i++)  { // populate root directory
+
+  			rootDirectory[i].iNode = -1;
+  		}
+
+      		init_fresh_disk(SYSTEMNAME, BLOCKSIZE, 2048); // creating new file system
+
+      	 	int *superBuffer = malloc(BLOCKSIZE);
+      	 	write_blocks(SUPERBLOCK, 1, superBuffer);
+      	 	free(superBuffer);
+
+  	 	superBlock.magic = 0;
+  	 	superBlock.blockSize = BLOCK_SIZE;
+  	 	superBlock.fileSystemSize = 2048;
+  	 	superBlock.iNodeTable = 500;
+  	 	superBlock.rootDirectory = *rootDirectory;
 
   	}
-
-      	for (int i = 0; i < 500; i++)  { // populate root directory
-
-  		rootDirectory[i].iNode = -1;
-  	}
-
-      	init_fresh_disk(SYSTEMNAME, BLOCKSIZE, 2048); // creating new file system
-
-      	 int *superBuffer = malloc(BLOCKSIZE);
-      	 write_blocks(SUPERBLOCK, 1, superBuffer);
-      	 free(superBuffer);
-
-  	 superBlock.magic = 0;
-  	 superBlock.blockSize = BLOCK_SIZE;
-  	 superBlock.fileSystemSize = 2048;
-  	 superBlock.iNodeTable = 500;
-  	 superBlock.rootDirectory = *rootDirectory;
-
-  }
 
 }
 
